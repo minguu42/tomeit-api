3edc813d5ed42bfe309fb52cb0d120b2bad0ec6f
minguu <minguu42@gmail.com>
2021-06-22 10:15:50 +0900

    errInvalidRequest, errRender の追加

---
+++ error.go
@@ -0,0 +1,38 @@
+package tomeit
+
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+)
+
+type ErrResponse struct {
+	Err            error `json:"-"`
+	HTTPStatusCode int   `json:"-"`
+
+	StatusText string `json:"status"`
+	ErrorText  string `json:"error,omitempty"`
+}
+
+func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	render.Status(r, e.HTTPStatusCode)
+	return nil
+}
+
+func errInvalidRequest(err error) render.Renderer {
+	return &ErrResponse{
+		Err:            err,
+		HTTPStatusCode: 400,
+		StatusText:     "Invalid request",
+		ErrorText:      err.Error(),
+	}
+}
+
+func errRender(err error) render.Renderer {
+	return &ErrResponse{
+		Err:            err,
+		HTTPStatusCode: 422,
+		StatusText:     "Error rendering response",
+		ErrorText:      err.Error(),
+	}
+}

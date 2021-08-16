83b393a53a02a4bea4f86b72fea62b3254d15671
minguu <minguu42@gmail.com>
2021-08-16 20:53:34 +0900

    テーブル定義の変更

--- build/setup.sql
+++ build/setup.sql
@@ -1,28 +1,34 @@
 SET CHARACTER SET UTF8;
 
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS pomodoro_records;
+
 CREATE TABLE IF NOT EXISTS users (
-    id              INT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    digest_uid      CHAR(64) NOT NULL,
-    rest_count      INT      DEFAULT 4 NOT NULL CHECK ( 1 <= rest_count AND rest_count <= 4 )
+    id              INT       PRIMARY KEY AUTO_INCREMENT,
+    digest_uid      CHAR(64)  NOT NULL UNIQUE,
+    next_rest_count INT       DEFAULT 4 NOT NULL CHECK ( 1 <= rest_count AND rest_count <= 4 ),
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tasks (
-    id         INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id    INT          NOT NULL,
-    name       VARCHAR(120) NOT NULL,
-    priority   INT          DEFAULT 0 NOT NULL CHECK (0 <= priority AND priority <= 3),
-    deadline   DATE         DEFAULT ('0001-01-01') NOT NULL,
-    is_done    BOOLEAN      DEFAULT FALSE NOT NULL,
-    created_at TIMESTAMP    DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    id                       INT          PRIMARY KEY AUTO_INCREMENT,
+    user_id                  INT          NOT NULL,
+    title                    VARCHAR(120) NOT NULL,
+    expected_pomodoro_number INT          DEFAULT 0 NOT NULL CHECK (0 <= priority AND priority <= 3),
+    due_on                   DATE         DEFAULT ('0001-01-01') NOT NULL,
+    is_completed             BOOLEAN      DEFAULT FALSE NOT NULL,
+    completed_at             TIMESTAMP    DEFAULT ('0001-01-01 00:00:00') NOT NULL,
+    created_at               TIMESTAMP    DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at               TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE IF NOT EXISTS pomodoro_logs (
-    id         INT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id    INT       NOT NULL,
-    task_id    INT       NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+CREATE TABLE IF NOT EXISTS pomodoro_records (
+    id           INT       PRIMARY KEY AUTO_INCREMENT,
+    user_id      INT       NOT NULL,
+    task_id      INT       NOT NULL,
+    completed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (task_id) REFERENCES tasks(id)
 );

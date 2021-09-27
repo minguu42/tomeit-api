ae9bf493892ce7c0b97463758eec640e6beb6f2d
minguu <minguu42@gmail.com>
2021-09-27 10:34:30 +0900

    getPomodorosByUser の実装方法を変更

--- pomodoros_db.go
+++ pomodoros_db.go
@@ -38,20 +38,40 @@ type getPomodorosOptions struct {
 }
 
 func (db *DB) getPomodorosByUser(user *User, options *getPomodorosOptions) ([]Pomodoro, error) {
-	q := db.Select("pomodoros.id, pomodoros.user_id, pomodoros.task_id, tasks.id, tasks.title, tasks.expected_pomodoro_num, tasks.due_at, tasks.is_completed, tasks.completed_at, tasks.created_at, tasks.updated_at, pomodoros.created_at").Joins("JOIN tasks ON pomodoros.task_id = tasks.id").Where("user_id = ?", user.ID)
+	const q = `
+SELECT P.id, P.created_at, T.id, T.title, T.expected_pomodoro_num, T.due_at, T.is_completed, T.completed_at, T.created_at, T.updated_at
+FROM pomodoros AS P
+JOIN tasks AS T ON P.task_id = T.id
+WHERE P.user_id = ?
+ORDER BY P.created_at
+LIMIT 30
+`
 
-	if options != nil {
-		if options.existCompletedOn {
-			y, m, d := options.completedOn.Date()
-			start := time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
-			end := time.Date(y, m, d, 23, 59, 59, 0, time.UTC)
-			q = q.Where("completed_at BETWEEN ? AND ?", start, end)
-		}
+	//if options != nil {
+	//	if options.existCompletedOn {
+	//		y, m, d := options.completedOn.Date()
+	//		start := time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
+	//		end := time.Date(y, m, d, 23, 59, 59, 0, time.UTC)
+	//		q = q.Where("completed_at BETWEEN ? AND ?", start, end)
+	//	}
+	//}
+
+	rows, err := db.Raw(q, user.ID).Rows()
+	if err != nil {
+		return nil, fmt.Errorf("db.Rows failed: %w", err)
 	}
 
 	var pomodoros []Pomodoro
-	if err := q.Order("created_at").Limit(30).Find(&pomodoros).Error; err != nil {
-		return nil, fmt.Errorf("db.Find failed: %w", err)
+	for rows.Next() {
+		var p Pomodoro
+		if err := rows.Scan(&p.ID, &p.CreatedAt, &p.Task.ID, &p.Task.Title, &p.Task.ExpectedPomodoroNum, &p.Task.DueAt, &p.Task.IsCompleted, &p.Task.CompletedAt, &p.Task.CreatedAt, &p.Task.UpdatedAt); err != nil {
+			return nil, fmt.Errorf("rows.Scan failed: %w", err)
+		}
+		pomodoros = append(pomodoros, p)
+	}
+
+	if err := rows.Close(); err != nil {
+		return nil, fmt.Errorf("rows.Close faield")
 	}
 
 	return pomodoros, nil
